ba25779c3dfcaceb220e7c90339cb861a5ca04c1
michaelhly <michaelhly@gmail.com>
2022-06-12 23:28:41 -0400

    Add resize event listener

--- src/world/systems/Resizer.ts
+++ src/world/systems/Resizer.ts
@@ -2,16 +2,24 @@ import { PerspectiveCamera, WebGLRenderer } from "three";
 
 export class Resizer {
     constructor(container: HTMLElement, camera: PerspectiveCamera, renderer: WebGLRenderer) {
-        // Set the camera's aspect ratio
-        camera.aspect = container.clientWidth / container.clientHeight;
-    
-        // update the camera's frustum
-        camera.updateProjectionMatrix();
-    
-        // update the size of the renderer AND the canvas
-        renderer.setSize(container.clientWidth, container.clientHeight);
-    
-        // set the pixel ratio (for mobile devices)
-        renderer.setPixelRatio(window.devicePixelRatio);
+        // set initial size on load
+        setSize(container, camera, renderer)
+
+        window.addEventListener('resize', () => {
+            // set the size again if a resize occurs
+            setSize(container, camera, renderer);
+        })
     }
 }
+
+const setSize = (container: HTMLElement, camera: PerspectiveCamera, renderer: WebGLRenderer) => {
+    // Set the camera's aspect ratio
+    camera.aspect = container.clientWidth / container.clientHeight;
+    // update the camera's frustum
+    camera.updateProjectionMatrix();
+
+    // update the size of the renderer AND the canvas
+    renderer.setSize(container.clientWidth, container.clientHeight);
+    // set the pixel ratio (for mobile devices)
+    renderer.setPixelRatio(window.devicePixelRatio);
+  };
