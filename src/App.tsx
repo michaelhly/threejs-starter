75e26300f0733b75e5d11a6778289a34d1b2af1c
michaelhly <michaelhly@gmail.com>
2022-06-15 17:17:43 -0400

    Set camera params

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,21 @@
 import { Canvas } from "@react-three/fiber";
 import { Box } from "./component/Box";
+import Flamingo from "./component/Flamingo";
 import { MainLights } from "./component/MainLights";
+import Parrot from "./component/Parrot";
+import Stork from "./component/Stork";
 
 function App() {
   return (
-    <Canvas>
+    <Canvas
+      camera={{
+        fov: 35,
+        aspect: 1,
+        near: 0.1,
+        far: 100,
+        position: [-1.5, 1.5, 6],
+      }}
+    >
       <MainLights />
       <Box position={[-1.2, 0, 0]} />
       <Box position={[1.2, 0, 0]} />
